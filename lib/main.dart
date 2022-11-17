c898a27124a5a636a9301e720b7a912a91941b2b
snehal <snehal.hashtechy@gmail.com>
2022-11-17 16:28:28 +0530

    added

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:push_notification_demo/screens/home_screen.dart';
+import 'package:push_notification_demo/services/local_notification_services.dart';
+
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  LocalNotificationService.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notificatios',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  HomeScreen(),
+    );
+  }
+}
+
